964c626a5e8d4233b7cee348356975d1a2b9b4c7
mugt <nwck.thomas@gmail.com>
2017-03-04 11:19:42 +0100

    normal-reponce exo sur normal a une sphere ok

--- devoir_1_normale-reponce.c
+++ devoir_1_normale-reponce.c
@@ -41,7 +41,8 @@ void clavier(unsigned char key){}
 void redimension(int width, int height){}
 
 void wireSphere(double radius, int slice,int stack){
-	double theta,phi,cost,costs,sint,sints,sinp,sinps,z,zs;
+	double radius2 = radius + 0.2;
+	double theta,phi,cost,cost2,costs,costs2,sint,sint2,sints,sints2,sinp,sinps,z,z2,zs;
 	int i,j;
 	double incrSlice =  2 * M_PI / (double) slice;
 	double incrStack =   M_PI / (double) stack;
@@ -50,17 +51,29 @@ void wireSphere(double radius, int slice,int stack){
 	double p0[]={0,0,0};
 	for(i=0;i<slice;i++){
 		cost=radius*cos(theta);sint=radius*sin(theta);
+		cost2=radius2*cos(theta);sint2=radius2*sin(theta);
+
 		costs=radius*cos(theta+incrSlice);sints=radius*sin(theta+incrSlice);
+		costs2=radius2*cos(theta+incrSlice);sints2=radius2*sin(theta+incrSlice);
+
 		phi=0;
 		for(j=0;j<=stack;j++){
 			z = radius * cos(phi); sinp = sin(phi);
+			z2 = radius2 * cos(phi); sinp = sin(phi);
 			zs = radius * cos(phi+incrStack);sinps=sin(phi+incrStack);
+			//void affiche_vecteur(double origine, vecteur, VERT)//origine, vecteur, couleur
 			// Calcule et affichage de la normale � �crire.
 			glBegin(GL_LINE_LOOP);
 				glVertex3d(cost*sinp,sint*sinp,z);
 				glVertex3d(cost*sinps,sint*sinps,zs);
 				glVertex3d(costs*sinps,sints*sinps,zs);
 				glVertex3d(costs*sinp,sints*sinp,z);
+			glEnd();
+			glBegin(GL_LINES);
+				glColor3dv(NOIR);
+				glVertex3d(cost*sinp,sint*sinp,z);
+				glVertex3d(cost2*sinp,sint2*sinp,z2);
+				glColor3dv(ROUGE);
 			glEnd();
 			phi= phi + incrStack;
 		}
